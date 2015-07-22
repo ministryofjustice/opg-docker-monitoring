4fa5e47f508b159f33f3300624a6b0dc952484ca
dreay-moj <david.reay@devopsguys.com>
2015-07-22 14:33:27 +0100

    dashboard script to automate instance metric dashboards

--- grafana/docker/grafana/public/dashboards/instance.js
+++ grafana/docker/grafana/public/dashboards/instance.js
@@ -11,14 +11,14 @@
  *
  * For async scripts, return a function, this function must take a single callback function,
  * call this function with the dasboard object
- *
+ * 
  * Author: Anatoliy Dobrosynets, Recorded Future, Inc.
  */
 // accessable variables in this scope
 var window, document, ARGS, $, jQuery, moment, kbn;
 
 // use defaults for URL arguments
-var arg_env = 'ns';
+var arg_env = 'metrics';
 var arg_i = '*';
 var arg_span = 4;
 var arg_from = '2h';
@@ -27,27 +27,22 @@ var arg_host = 'http://localhost:8003';
 if (!_.isUndefined(ARGS.span)) {
     arg_span = ARGS.span;
 }
-
 if (!_.isUndefined(ARGS.from)) {
     arg_from = ARGS.from;
 }
-
 if (!_.isUndefined(ARGS.env)) {
     arg_env = ARGS.env;
 }
-
 if (!_.isUndefined(ARGS.i)) {
     arg_i = ARGS.i;
 }
-
 if (!_.isUndefined(ARGS.host)) {
     arg_host = ARGS.host;
 }
 
 var metric_filter = arg_env + "." + arg_i;
 
-// return dashboard filter_list
-// optionally include 'All'
+// helper functions
 function get_filter_object(name, query, show_all) {
     show_all = (typeof show_all === "undefined") ? true : show_all;
     var arr = find_filter_values(query);
@@ -57,13 +52,13 @@ function get_filter_object(name, query, show_all) {
             "text": arr[i],
             "value": arr[i]
         });
-    };
-    if (show_all == true) {
+    }
+    if (show_all === true) {
         opts.unshift({
             "text": "All",
             "value": '{' + arr.join() + '}'
         });
-    };
+    }
     return {
         type: "filter",
         name: name,
@@ -72,10 +67,7 @@ function get_filter_object(name, query, show_all) {
         current: opts[0],
         includeAll: show_all
     }
-};
-
-// execute graphite-api /metrics/find query
-// return array of metric last names ( func('test.cpu-*') returns ['cpu-0','cpu-1',..] )
+}
 function find_filter_values(query) {
     var search_url = arg_host + '/metrics/find/?query=' + query;
     var res = [];
@@ -89,10 +81,7 @@ function find_filter_values(query) {
         }
     }
     return res;
-};
-
-// execute graphite-api /metrics/expand query
-// return array of metric full names (func('*.cpu-*') returns ['test.cpu-0','test.cpu-1',..] )
+}
 function expand_filter_values(query) {
     var search_url = arg_host + '/metrics/expand/?query=' + query;
     var req = new XMLHttpRequest();
@@ -103,12 +92,17 @@ function expand_filter_values(query) {
         return obj['results'];
     } else {
         return [];
-    };
-};
+    }
+}
+function len(prefix) {
+    return prefix.split('.').length - 2;
+}
 
-//---------------------------------------------------------------------------------------
-
-function panel_collectd_delta_cpu(title, prefix) {
+/* 
+  panel templates
+*/
+function panel_collectd_cpu(title, prefix) {
+    var idx = len(prefix);
     return {
         title: title,
         type: 'graphite',
@@ -120,7 +114,7 @@ function panel_collectd_delta_cpu(title, prefix) {
             min: 0
         },
         lines: true,
-        fill: 2,
+        fill: 1,
         linewidth: 1,
         stack: true,
         legend: {
@@ -133,34 +127,33 @@ function panel_collectd_delta_cpu(title, prefix) {
             query_as_alias: true
         },
         targets: [{
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.interrupt),'intrpt')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.user,0)),'user')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.softirq),'irq')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.system,0)),'system')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.steal),'steal')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.idle,0)),'idle')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.wait),'wait')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.wait,0)),'wait')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.system),'system')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.steal,0)),'steal')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.user),'user')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.nice,0)),'nice')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.nice),'nice')"
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.softirq,0)),'irq')"
         }, {
-            "target": "alias(sumSeries(" + metric_filter + ".cpu.*.cpu.idle),'idle')"
-        }, ],
+            "target": "alias(sumSeries(nonNegativeDerivative(" + metric_filter + ".cpu.*.cpu.interrupt,0)),'intrpt')"
+        } ],
         aliasColors: {
             "user": "#508642",
-            "nice": "#609652",
             "system": "#EAB839",
             "wait": "#890F02",
             "steal": "#E24D42",
             "idle": "#6ED0E0"
         }
     }
-};
-
+}
 function panel_collectd_memory(title, prefix) {
+    var idx = len(prefix);
     return {
         title: title,
         type: 'graphite',
@@ -171,23 +164,29 @@ function panel_collectd_memory(title, prefix) {
             min: 0
         },
         lines: true,
-        fill: 2,
+        fill: 1,
         linewidth: 1,
         stack: true,
         nullPointMode: "null",
         targets: [{
-            "target": "aliasByNode(" + metric_filter + ".memory.memory.{used,cached,buffered,free},-1)"
-        }, ],
+            "target": "alias(movingMedian(" + metric_filter + ".memory.memory.buffered,'15min'),'buffered')"
+        },{
+            "target": "alias(movingMedian(" + metric_filter + ".memory.memory.cached,'15min'),'cached')"
+        },{
+            "target": "alias(movingMedian(" + metric_filter + ".memory.memory.free,'15min'),'free')"
+        },{
+            "target": "alias(movingMedian(" + metric_filter + ".memory.memory.used,'15min'),'used')"
+        }],
         aliasColors: {
-            "free": "#629E51",
-            "used": "#1F78C1",
-            "cached": "#EF843C",
-            "buffered": "#CCA300"
+            "memory-free": "#629E51",
+            "memory-used": "#1F78C1",
+            "memory-cached": "#EF843C",
+            "memory-buffered": "#CCA300"
         }
     }
-};
-
+}
 function panel_collectd_loadavg(title, prefix) {
+    var idx = len(prefix);
     return {
         title: title,
         type: 'graphite',
@@ -198,25 +197,22 @@ function panel_collectd_loadavg(title, prefix) {
             min: 0
         },
         lines: true,
-        fill: 2,
-        linewidth: 1,
+        fill: 0,
+        linewidth: 2,
         nullPointMode: "null",
         targets: [{
-            "target": "aliasByNode(movingMedian(" + metric_filter + ".load.load.longterm,'10min'),-1)"
+            "target": "alias(movingMedian(" + metric_filter + ".load.load.longterm,'10min'),'longterm')"
         },{
-            "target": "aliasByNode(movingMedian(" + metric_filter + ".load.load.midterm,'10min'),-1)"
+            "target": "alias(movingMedian(" + metric_filter + ".load.load.midterm,'10min'),'midterm')"
         },{
-            "target": "aliasByNode(movingMedian(" + metric_filter + ".load.load.shortterm,'10min'),-1)"
-        }],
-        aliasColors: {
-            "cpuCount": "green",
-            "midterm": "red"
-        }
+            "target": "alias(movingMedian(" + metric_filter + ".load.load.shortterm,'10min'),'shortterm')"
+        } ]
     }
-};
+}
 
 function panel_collectd_network_octets(title, prefix, intrf) {
     intrf = (typeof intrf === "undefined") ? 'eth0' : intrf;
+    var idx = len(prefix);
     return {
         title: title + ', ' + intrf,
         type: 'graphite',
@@ -231,15 +227,15 @@ function panel_collectd_network_octets(title, prefix, intrf) {
         linewidth: 2,
         nullPointMode: "null",
         targets: [{
-            "target": "aliasByNode(movingMedian(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_octets.rx,10),'5min'),-3,-1)"
+            "target": "alias(movingMedian(nonNegativeDerivative(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_octets.rx,10),0),'5min'),'rx')"
         }, {
-            "target": "aliasByNode(movingMedian(scale(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_octets.tx,10),-1),'5min'),-3,-1)"
+            "target": "alias(movingMedian(scale(nonNegativeDerivative(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_octets.tx,10),0),-1),'5min'),'tx')"
         }]
     }
-};
-
+}
 function panel_collectd_network_packets(title, prefix, intrf) {
     intrf = (typeof intrf === "undefined") ? 'eth0' : intrf;
+    var idx = len(prefix);
     return {
         title: title + ', ' + intrf,
         type: 'graphite',
@@ -254,15 +250,15 @@ function panel_collectd_network_packets(title, prefix, intrf) {
         linewidth: 2,
         nullPointMode: "null",
         targets: [{
-            "target": "aliasByNode(movingMedian(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_packets.rx,10),'5min'),-3,-1)"
+            "target": "alias(movingMedian(nonNegativeDerivative(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_packets.rx,10),0),'5min'),'rx')"
         }, {
-            "target": "aliasByNode(movingMedian(scale(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_packets.tx,10),-1),'5min'),-3,-1)"
+            "target": "alias(movingMedian(scale(nonNegativeDerivative(keepLastValue(" + metric_filter + ".interface." + intrf + ".if_packets.tx,10),0),-1),'5min'),'tx')"
         }]
     }
-};
-
+}
 function panel_collectd_df(title, prefix, vol) {
-    vol = (typeof vol === "undefined") ? 'root' : vol;
+    vol = (typeof vol === "undefined") ? 'df-root' : vol;
+    var idx = len(prefix);
     return {
         title: title + ', ' + vol,
         type: 'graphite',
@@ -279,18 +275,22 @@ function panel_collectd_df(title, prefix, vol) {
         stack: true,
         nullPointMode: "null",
         targets: [{
-            "target": "aliasByNode(" + metric_filter + ".df." + vol + ".df_complex.{free,used,reserved},-3,-1)"
-        }, ],
+            "target": "alias(" + metric_filter + ".df." + vol + ".df_complex.free,'free')"
+            },{
+            "target": "alias(" + metric_filter + ".df." + vol + ".df_complex.used,'used')"
+            },{
+            "target": "alias(" + metric_filter + ".df." + vol + ".df_complex.reserved,'reserved')"
+        }],
         aliasColors: {
-            "used": "#447EBC",
-            "free": "#508642",
-            "reserved": "#EAB839"
+            "df_complex-used": "#447EBC",
+            "df_complex-free": "#508642",
+            "df_complex-reserved": "#EAB839"
         }
     }
-};
-
+}
 function panel_collectd_disk(title, prefix, vol) {
     vol = (typeof vol === "undefined") ? 'sda' : vol;
+    var idx = len(prefix);
     return {
         title: title + ', ' + vol,
         type: 'graphite',
@@ -301,25 +301,25 @@ function panel_collectd_disk(title, prefix, vol) {
             min: null
         },
         lines: true,
-        fill: 2,
-        linewidth: 1,
+        fill: 1,
+        linewidth: 2,
         nullPointMode: "null",
         targets: [{
-            "target": "aliasByNode(" + metric_filter + ".disk." + vol + ".disk_ops.write,-3,-1)"
+            "target": "alias(nonNegativeDerivative(" + metric_filter + ".disk." + vol + ".disk_ops.write,10),'write')"
         }, {
-            "target": "aliasByNode(scale(" + metric_filter + ".disk." + vol + ".disk_ops.read,-1),-3,-1)"
+            "target": "alias(scale(nonNegativeDerivative(" + metric_filter + ".disk." + vol + ".disk_ops.read,10),-1),'read')"
         }],
         aliasColors: {
-            "write": "#447EBC",
-            "read": "#508642",
+            "df_complex-used": "#447EBC",
+            "df_complex-free": "#508642",
+            "df_complex-reserved": "#EAB839"
         }
     }
-};
+}
 
 /*
   row templates
 */
-
 function row_delimiter(title) {
     return {
         title: "_____ " + title,
@@ -335,23 +335,21 @@ function row_delimiter(title) {
             mode: "text"
         }]
     }
-};
-
+}
 function row_cpu_memory(title, prefix) {
     return {
         title: title,
         height: '250px',
         collapse: false,
         panels: [
-            panel_collectd_delta_cpu('CPU, %', prefix),
+            panel_collectd_cpu('CPU, %', prefix),
             panel_collectd_memory('Memory', prefix),
             panel_collectd_loadavg('Load avg, 10min', prefix)
         ]
     }
-};
-
+}
 function row_network(title, prefix, filter) {
-    var interfaces = find_filter_values(filter + '.interface.*');
+    var interfaces = find_filter_values(metric_filter+ '.interface.*');
     var panels_network = [];
     for (var i in interfaces) {
         if ( ! /^eth\d/.test(interfaces[i]) ) { continue; }
@@ -366,59 +364,40 @@ function row_network(title, prefix, filter) {
         collapse: true,
         panels: panels_network
     }
-};
-
+}
 function row_disk_space(title, prefix, filter) {
-    var volumes = find_filter_values(filter + '.df.*');
+    var volumes = ['srv','data','root'];
     panels_disk_space = [];
     for (var i in volumes) {
-        if ( ! /root|srv|data/.test(volumes[i]) ) { continue; }
         panels_disk_space.push(panel_collectd_df('disk space', prefix, volumes[i]));
-    };
+    }
     return {
         title: title,
         height: '250px',
         collapse: true,
         panels: panels_disk_space
     }
-};
-
+}
 function row_disk_usage(title, prefix, filter) {
-    var volumes = find_filter_values(filter + '.disk.*');
+    var volumes = find_filter_values(metric_filter + '.disk.*');
     var panels_disk_usage = [];
     for (var i in volumes) {
-        if ( /\d/.test(volumes[i]) ) { continue; }
+       if ( /\d/.test(volumes[i]) ) { continue; } 
         panels_disk_usage.push(panel_collectd_disk('disk ops read/write', prefix, volumes[i]));
-    };
+    }
     return {
         title: title,
         height: '250px',
         collapse: true,
         panels: panels_disk_usage
     }
-};
-
-//---------------------------------------------------------------------------------------
-
+}
 
 return function(callback) {
 
-    // Setup some variables
     var dashboard;
-
-    /* prefix - depends on actual Graphite tree.
-                In my case it depends on environment which can be passed as argument too.
-        .collectd.hosts.
-        .statsd.hosts.
-        .jmxtrans.hosts.
-    */
-
-    //var prefix = arg_env + '.collectd.hosts.';
-    var prefix = arg_env + '.';
-
+    var prefix = arg_env + '.collectd.hosts.';
     var arg_filter = prefix + arg_i;
-
-    // set filter
     var dashboard_filter = {
         time: {
             from: "now-" + arg_from,
@@ -429,7 +408,6 @@ return function(callback) {
         ]
     };
 
-    // define pulldowns
     pulldowns = [{
         type: "filtering",
         collapse: false,
@@ -441,7 +419,6 @@ return function(callback) {
     }];
 
     // Intialize a skeleton with nothing but a rows array and service object
-
     dashboard = {
         rows: [],
         services: {}
@@ -451,9 +428,7 @@ return function(callback) {
     dashboard.pulldowns = pulldowns;
     dashboard.services.filter = dashboard_filter;
 
-
     // custom dashboard rows (appended to the default dashboard rows)
-
     var optional_rows = [];
 
     $.ajax({
@@ -461,23 +436,15 @@ return function(callback) {
             url: '/'
         })
         .done(function(result) {
-
-            // costruct dashboard rows
-
             dashboard.rows.push(
                 row_cpu_memory('cpu, memory', prefix),
                 row_network('network', prefix, arg_filter),
                 row_disk_space('disk space', prefix, arg_filter),
                 row_disk_usage('disk ops', prefix, arg_filter)
             );
-
-            // custom rows
             for (var i in optional_rows) {
                 dashboard.rows.push(optional_rows[i]);
             };
-
-            // when dashboard is composed call the callback
-            // function and pass the dashboard
             callback(dashboard);
         });
 }

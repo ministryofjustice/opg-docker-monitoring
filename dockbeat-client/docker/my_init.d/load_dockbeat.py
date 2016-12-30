05096ec8a39049e1364910da929afacdff8fb97a
Brett Minnie <github@bdb-studios.co.uk>
2016-12-30 08:59:18 +0000

    Added exception handling if we failed to connect and load the template

--- dockbeat-client/docker/my_init.d/load_dockbeat.py
+++ dockbeat-client/docker/my_init.d/load_dockbeat.py
@@ -6,20 +6,29 @@ import requests
 
 if os.getenv('BEATS_DOCKBEAT_MONITORING_HOST'):
 
-    base_path = os.getenv('GOPATH')
-    template_target = '/_template/dockbeat'
-    payload = json.load(
-        open(
-            os.path.abspath(base_path +
-                            '/src/github.com/ingensi/dockbeat/etc/dockbeat.template.json'
-                            )
+    try:
+        base_path = os.getenv('GOPATH')
+        template_target = '/_template/dockbeat'
+        payload = json.load(
+            open(
+                os.path.abspath(base_path +
+                                '/src/github.com/ingensi/dockbeat/etc/dockbeat.template.json'
+                                )
+            )
         )
-    )
 
-    r = requests.put("http://{}:9200{}".format(
-        os.getenv('BEATS_DOCKBEAT_MONITORING_HOST'),
-        template_target),
-        data=json.dumps(payload)
-    )
+        r = requests.put("http://{}:9200{}".format(
+            os.getenv('BEATS_DOCKBEAT_MONITORING_HOST'),
+            template_target),
+            data=json.dumps(payload)
+        )
 
-    print (r.text)
+        print (r.text)
+    except requests.ConnectionError as e:
+        print("Failed to connect to the ES server {}"
+              .format(os.getenv('BEATS_DOCKBEAT_MONITORING_HOST'))
+              )
+
+        print(e.message)
+    finally:
+        exit(0)
